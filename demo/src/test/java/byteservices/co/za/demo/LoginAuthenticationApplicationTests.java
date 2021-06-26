339da57dc4a96ff58733074b9d13eda150bcce42
randall.erasmus <randallerasmus1@gmail.com>
2021-06-26 23:46:09 +0200

    Initial commit

---
+++ demo/src/test/java/byteservices/co/za/demo/LoginAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package byteservices.co.za.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
