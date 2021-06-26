f4fa652fd2aa4c24c7bfd2b4ef605fe8dcd23641
randall.erasmus <randallerasmus1@gmail.com>
2021-06-27 01:56:22 +0200

    Registration completed

--- demo/src/main/java/byteservices/co/za/demo/appuser/AppUserRepository.java
+++ demo/src/main/java/byteservices/co/za/demo/appuser/AppUserRepository.java
@@ -2,6 +2,7 @@ package byteservices.co.za.demo.appuser;
 
 // this is a way to have a query against our database
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +10,6 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
 }
