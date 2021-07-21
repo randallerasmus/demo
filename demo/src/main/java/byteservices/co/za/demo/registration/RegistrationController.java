a7b64426dcd882de44903b98c0f36d142e7406e6
randall.erasmus <randallerasmus1@gmail.com>
2021-07-21 23:59:55 +0200

    database structures - disable the mail funtion

--- demo/src/main/java/byteservices/co/za/demo/registration/RegistrationController.java
+++ demo/src/main/java/byteservices/co/za/demo/registration/RegistrationController.java
@@ -17,7 +17,7 @@ public class RegistrationController {
         return registrationService.register(request);
     }
 
-    @GetMapping(path = "confirm")
+    @GetMapping(path = "confirms")
     public String confirm(@RequestParam("token") String token) {
         return registrationService.confirmToken(token);
     }
